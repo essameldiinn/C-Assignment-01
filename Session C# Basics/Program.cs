27025724295bcc33d8911f1a9cdd6e714001278f
essameldiinn <essamalaa2003@gmail.com>
2025-03-15 05:11:53 +0200

    Q2 - Q8 is done

--- Session C# Basics/Program.cs
+++ Session C# Basics/Program.cs
@@ -9,6 +9,72 @@
             //int number = Convert.ToInt32(Console.ReadLine());
             //Console.WriteLine(number); 
             #endregion
+            #region Q2 
+            //Console.WriteLine("Enter Your String ... :");
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(num);
+
+            //Unhandled exception
+            #endregion
+
+            #region Q3
+            //int x = 5;
+            //float y = 0.000005f;
+            //Console.WriteLine($"result is {x + y}"); 
+            #endregion
+
+            #region Q4
+            //Console.Write("Enter a string: ");
+            //string? String1 = Console.ReadLine();
+
+            //Console.Write("Enter the starting index: ");
+            //int startIndex = int.Parse(Console.ReadLine() ?? "0");
+
+            //Console.Write("Enter the length of the substring: ");
+            //int length = int.Parse(Console.ReadLine()?? "0");
+
+            //if (startIndex < 0 || startIndex >= String1.Length || startIndex + length > String1.Length)
+            //{
+            //    Console.WriteLine("Invalid index or length.");
+            //}
+            //else
+            //{
+            //    string extractedSubstring = String1.Substring(startIndex, length);
+            //    Console.WriteLine($"Extracted Substring: {extractedSubstring}");
+            //} 
+            #endregion
+
+            #region Q5
+            //int x = 2;
+            //int y = 3;
+
+            //x = y;
+
+            //Console.WriteLine(x);
+            //Console.WriteLine(y); 
+            #endregion
+
+            #region Q6
+            //Point p1 = new Point(); 
+            //Point p2 = new Point();
+
+            //p1.x = 5;
+            //p1.y = 6;
+            //p2.x = 7;
+            //p2.y = 8;
+
+            //p1 = p2;
+
+            //Console.WriteLine(p1.x); 
+            #endregion
+
+            #region Q7
+            //string name1 = "Essam";
+            //string name2 = "Alaa";
+
+            //Console.WriteLine($"My name is : {name1} {name2}"); 
+            #endregion
+
         }
     }
 }
